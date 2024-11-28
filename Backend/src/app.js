5f6d833c576fda602338194fd6805400879c64f3
Kunal8615 <113933312+Kunal8615@users.noreply.github.com>
2024-11-28 11:31:56 +0530

    origin

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -9,10 +9,12 @@ app.use(
         origin: [
                 "http://localhost:5173",
                 "tube-tweet-mu.vercel.app",
-               "https://server-alpha-gray-79.vercel.app",
+            
              //  "https://tubetweet.onrender.com"
               ],
     credentials: true,
+    methods: ["GET", "POST", "PUT", "DELETE"],
+
   })
 );
 //app.options('*', cors());
