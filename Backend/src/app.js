4b053d4089de0fc684e33f5c6eeb56fcbfaa94a2
Kunal8615 <113933312+Kunal8615@users.noreply.github.com>
2024-12-01 11:10:12 +0530

    fd

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -7,7 +7,7 @@ const app = express();
 app.use(
   cors({
         origin: [
-               "http://localhost:5173/",
+               "http://localhost:5173",
                 "https://tube-tweet-mu.vercel.app",
             
              //  "https://tubetweet.onrender.com"
