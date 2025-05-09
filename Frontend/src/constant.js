5273fae5abf4c8e396af9790066dde14d377468e
Kunal8615 <113933312+Kunal8615@users.noreply.github.com>
2025-05-09 17:07:47 +0530

    changes  api

--- Frontend/src/constant.js
+++ Frontend/src/constant.js
@@ -1,5 +1,5 @@
 
-export const API_URL = 'http://localhost:8000/api/v1';
+export const API_URL = 'https://tubetweet.onrender.com/api/v1';
 //http://localhost:8000/api/v1
 //'https://tubetweet.onrender.com/api/v1'
 
